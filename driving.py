4dd39132edf903abf568c59a467257e5f304c1fc
JoanneHsu <a23073788@gmail.com>
2019-02-17 16:51:18 +0800

    Add new flie

---
+++ driving.py
@@ -0,0 +1,17 @@
+country = input('請問您是哪國人:')
+age = input('請問您幾歲:')
+if country == "台灣" or country == "韓國" or country == "新加坡":
+	if int(age) >= 18:
+		print('您可以開車')
+	else:
+		print('你不能開車')
+elif country == "美國" or  country == "加拿大" or  country == "日本" or  country == "英國":
+	if int(age) >= 16:
+		print('您可以開車')
+	else:
+		print('您不能開車')
+elif country == "泰國":
+	if int(age) >= 15:
+		print('您可以開車')
+	else:
+		print('您不能開車')
